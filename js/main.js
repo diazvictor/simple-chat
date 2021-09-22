baadd8e046eaae8848ec9e7fcfee6b435de50806
diazvictor <victor.vector008@gmail.com>
2021-09-21 22:21:32 -0400

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/**!
+ * @package   Simple Chat
+ * @filename  main.js
+ * @version   1.0
+ * @autor     Díaz Urbaneja Víctor Eduardo Diex <diazvictor@tutamail.com>
+ * @date      21.09.2021 22:20:32 -04
+ */
